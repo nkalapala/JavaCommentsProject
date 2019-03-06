5920ca3b19e14f092a6a741ca0bda870c5b1a21b
nkalapala <narmada_mikki@hotmail.com>
2019-03-06 12:44:39 +0000

    JavaCommentsProject

---
+++ src/MyComments.java
@@ -0,0 +1,12 @@
+//this is beginning of class definition
+public class MyComments {//this is class def beginning
+    public static void main(String[] args) {//this is main method
+        System.out.println("today learning comments");
+        //this is end of main
+
+        /*hjffj
+        hffjfj
+        hfjfj
+         */
+    }
+}
